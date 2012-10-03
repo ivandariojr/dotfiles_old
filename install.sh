20e1ca6892230f2ab1303baad8b19accfc3da32b
Saul Reynolds-Haertle <sreynoldshaertle@gmail.com>
2012-10-03 17:22:56 -0400

    Add awesome rc and section to install awesome and get awesome git repos

--- install.sh
+++ install.sh
@@ -21,3 +21,29 @@ do
 done
 
 [[ "$(ls -A $HOME/old-dotfiles)" ]] && echo "Copied old dotfiles to $HOME/old-dotfiles" || rm -rf "$HOME/old-dotfiles"
+
+#############################################
+########### Install other things ############
+#############################################
+
+### awesome
+if [ ! -d "~/.config/awesome" ]; then
+    mkdir "~/.config/awesome"
+fi
+
+if [ ! -d "~/.config/awesome/revelation" ]; then
+    git clone git://github.com/bioe007/awesome-revelation.git ~/.config/awesome/revelation
+fi
+
+if [ ! -d "~/.config/awesome/themes/" ]; then
+    mkdir "~/.config/awesome/themes"
+fi
+
+if [ ! -d "~/.config/awesome/themes/awesome-solarized" ]; then
+    git clone git://github.com/cycojesus/awesome-solarized.git ~/.config/awesome/themes/awesome-solarized
+fi
+
+[[ -e "$HOME/.config/awesome/rc.lua" ]] && [[ ! -h "$HOME/.config/awesome/rc.lua" ]] && mv "$HOME/.config/awesome/rc.lua" "$HOME/old-dotfiles/awesomerc.lua"
+[[ -e "$HOME/.config/awesome/rc.lua" ]] && [[ -h "$HOME/.config/awesome/rc.lua" ]] && rm "$HOME/.config/awesome/rc.lua"
+
+ln -s "$PWD/awesomerc.lua" "$HOME/.config/awesome/rc.lua"
