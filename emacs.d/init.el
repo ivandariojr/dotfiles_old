6528aba6b34082f97e6cb19a8db7c83baa153514
Saul Reynolds-Haertle <saulrh@gatech.edu>
2013-04-01 17:18:11 -0400

    fix bit about enable-paredit-mode not existing

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -318,18 +318,12 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;; handle parentheses in lispy languages better
-(add-hook 'emacs-lisp-mode-hook
-          (lambda () (enable-paredit-mode)))
-(add-hook 'slime-mode-hook
-          (lambda () (enable-paredit-mode)))
-(add-hook 'slime-repl-mode-hook
-          (lambda () (enable-paredit-mode)))
-(add-hook 'lisp-mode-hook
-          (lambda () (enable-paredit-mode)))
-(add-hook 'lisp-interaction-mode-hook
-          (lambda () (enable-paredit-mode)))
-(add-hook 'scheme-mode-hook
-          (lambda () (enable-paredit-mode)))
+(add-hook 'emacs-lisp-mode-hook (lambda () (paredit-mode 1)))
+(add-hook 'slime-mode-hook (lambda () (paredit-mode 1)))
+(add-hook 'slime-repl-mode-hook (lambda () (paredit-mode 1)))
+(add-hook 'lisp-mode-hook (lambda () (paredit-mode 1)))
+(add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode 1)))
+(add-hook 'scheme-mode-hook (lambda () (paredit-mode 1)))
 
 ;; Stop SLIME's REPL from grabbing DEL, which is annoying when
 ;; backspacing over a '('
