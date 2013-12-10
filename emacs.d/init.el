f6d112d01e5026b64bba6468e648f727863c969d
Eric Huang <ehuang4@gmail.com>
2013-12-10 14:00:40 -0500

    Fix marmalade repository loading

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -7,7 +7,8 @@
 ;;; First, load up some packages
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . 
+               "http://melpa.milkbox.net/packages/"))
 (add-to-list 'package-archives 
              '("marmalade" .
                "http://marmalade-repo.org/packages/"))
