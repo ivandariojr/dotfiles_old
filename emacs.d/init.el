1d3cfb957fb3b42b9a8ccb5d14b331965a460fe5
Eric Huang <ehuang4@gmail.com>
2013-12-11 11:47:02 -0500

    windmove keybindings

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -230,6 +230,12 @@
 ;;; magit
 (global-set-key (kbd "C-x g") 'magit-status)
 
+;;; efficient window switching
+(global-set-key (kbd "C-S-h") 'windmove-left)
+(global-set-key (kbd "C-S-j") 'windmove-)
+(global-set-key (kbd "C-S-k") 'windmove-up)
+(global-set-key (kbd "C-S-l") 'windmove-right)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;                                                                         ;;
