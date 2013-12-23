a6c2890cbc31f6326562aa97956fc752718b8b68
Eric Huang <ehuang4@gmail.com>
2013-12-23 15:34:43 -0500

    emacs: Fix typo in windmove hotkeys

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -232,7 +232,7 @@
 
 ;;; efficient window switching
 (global-set-key (kbd "C-S-h") 'windmove-left)
-(global-set-key (kbd "C-S-j") 'windmove-)
+(global-set-key (kbd "C-S-j") 'windmove-down)
 (global-set-key (kbd "C-S-k") 'windmove-up)
 (global-set-key (kbd "C-S-l") 'windmove-right)
 
